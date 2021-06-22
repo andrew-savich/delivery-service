ab8b358e4a7a352982956cd6972e1db9dec1a663
andrew-savich <savichandrew@Gmail.com>
2021-06-22 23:32:08 +0300

    Store basic entity

---
+++ delivery-service/src/main/java/com/andrewsavich/exposit/deliveryservice/model/Store.java
@@ -0,0 +1,52 @@
+package com.andrewsavich.exposit.deliveryservice.model;
+
+import java.util.ArrayList;
+
+public class Store {
+	private int id;
+	private String title;
+	private String description;
+	private ArrayList<Product> products;
+	
+	public Store(int id, String title, String description, ArrayList<Product> products) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.products = products;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public ArrayList<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(ArrayList<Product> products) {
+		this.products = products;
+	}
+	
+	
+	
+}
