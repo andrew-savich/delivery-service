58e7e6183ac1e120811450e542d7c189d47e4464
andrew-savich <savichandrew@Gmail.com>
2021-06-28 02:39:40 +0300

    Hierarhy types

--- delivery-service/src/main/java/com/andrewsavich/exposit/deliveryservice/model/product/ProductType.java
+++ delivery-service/src/main/java/com/andrewsavich/exposit/deliveryservice/model/product/ProductType.java
@@ -1,5 +1,37 @@
 package com.andrewsavich.exposit.deliveryservice.model.product;
 
 public enum ProductType {
-	FOOD, CLOTHES, HOUSEHOLD, ELECTRONICS;
+	PRODUCT(null),
+		ELECTRONICS(PRODUCT),
+			SMARTPHONE(ELECTRONICS),
+			LAPTOP(ELECTRONICS),
+			TV(ELECTRONICS),
+		CLOTHES(PRODUCT),
+			TSHIRT(CLOTHES),
+			PANTS(CLOTHES),
+			PARKA(CLOTHES),
+		FOOD(PRODUCT),
+			MEAT(FOOD),
+			MILKY(FOOD),
+			BREAD(FOOD);
+
+	private ProductType parent = null;
+
+	private ProductType(ProductType parent) {
+		this.parent = parent;
+	}
+
+	public boolean is(ProductType otherType) {
+		if (otherType == null) {
+			return false;
+		}
+
+		for (ProductType type = this; type != null; type = type.parent) {
+			if (otherType == type) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
